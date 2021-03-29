4fc084bfbbca8b4a6e221258c8545f4678358e94
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-03-29 14:07:19 +0400

    show r

---
+++ src/r.js
@@ -0,0 +1,19 @@
+const { mb, o } = require("./mbo");
+function r(o, s) {
+  o.v(s + 1);
+}
+
+mb(
+  o,
+  { a: 0, b: 0 },
+  (o, s) => o.v(s, s.a),
+  r,
+  mb,
+  (o, s, v) => o.v({ ...s, a: v }),
+  mb,
+  (o, s) => o.v(s, s.b),
+  mb,
+  r,
+  mb,
+  (o, s, v) => o.v({ ...s, b: v })
+);
