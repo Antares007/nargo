3e291a8c47964529673c451b8da40e50c6958007
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-04-28 15:21:08 +0400

    renames

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,17 @@
 const { mb0, mb1 } = require("../mb");
+const r0 = (o) => C(o[0]);
 const example0 = (o) => {
   C(o[0], 1, 2, 3);
   C(mb0, o, document.body, npith, (o, np) => {
     console.log("np", np);
+
+    C(mb1, np, "click", r0, (np, ...args) => {
+      console.log("args", args);
+    });
+
     C(mb0, np, document.createTextNode("hello"), npith, (o) => C(o[1]));
     C(np[1], document.createTextNode("there"), npith);
+
     C(np[2]);
   });
 };
@@ -14,16 +21,20 @@ setTimeout(() => {
 const npith = (o, node) => {
   if (node.nodeType === 1) {
     const piths = [];
-    const f = (o, p) => piths.push(p);
-    const oc = [f];
-    for (let n of node.childNodes) C(npith, oc, n);
-    C(o[0], [nstart, mbn(nnode), nend, { node, piths, pc: 0, fmap: {} }]);
+    const opushpith = [(o, p) => piths.push(p)];
+    for (let n of node.childNodes) C(npith, opushpith, n);
+    C(o[0], [
+      start,
+      mbn(next),
+      end,
+      { node, piths, pc: 0, fmap: {}, olset: new Set() },
+    ]);
   } else {
     node =
       node.nodeType === 3
         ? node
         : document.createTextNode("NT:" + node.nodeType);
-    C(o[0], [nstart, void 0, nend, { node }]);
+    C(o[0], [start, void 0, end, { node }]);
   }
 };
 const handleEvent = (e) => {
@@ -36,8 +47,10 @@ const handleEvent = (e) => {
       0
     );
 };
-const nstart = (o, oc, type, ...args) => {
-  if (type === "listenerlisten") {
+const start = (o, oc, type, ...args) => {
+  return console.log(type, args);
+  if (type === null) {
+    o[3].olset.add(oc);
   } else {
     if (o[3].fmap[type] == null) {
       const ol = [
@@ -48,7 +61,7 @@ const nstart = (o, oc, type, ...args) => {
       ];
       o[3].fmap[type] = ol;
       o[3].node.addEventListener(type, ol[3]);
-      C(oc[0], ol, "listenerlisten");
+      C(oc[0], ol, null);
     } else {
       const ol = o[3].fmap[type];
       const i = ol[3].piths.indexOf(oc);
@@ -60,7 +73,7 @@ const nstart = (o, oc, type, ...args) => {
       } else ol[3].piths.splice(i, 1);
       ol[3].piths.splice(ol[3].pc, 0, oc);
       ol[3].pc++;
-      C(oc[0], ol, "listenerlisten");
+      C(oc[0], ol, null);
     }
   }
 };
@@ -74,7 +87,7 @@ const lend = (o, oc) => {
     ol[3].o[3].node.removeEventListener(ol[3].type, ol[3]),
       delete ol[3].o[3].fmap[ol[3].type];
 };
-const nend = (o) => {
+const end = (o) => {
   if (o[3].piths == null) return;
   for (let l = o[3].piths.length; l > o[3].pc; l--) {
     const np = o[3].piths.splice(o[3].pc, 1)[0];
@@ -91,7 +104,7 @@ const nend = (o) => {
     ol[3].pc = 0;
   }
 };
-const nnode = (o, np) => {
+const next = (o, np) => {
   const i = o[3].piths.indexOf(np);
   if (i < 0);
   else if (i < o[3].pc) return;
@@ -108,39 +121,3 @@ const mbn = (nar) => (o, a) => {
     C(mb0, o, a, nar);
   } else C(nar, o, a);
 };
-
-function start(o, oc, type, nar, ...args) {
-  const listener = { handleEvent, end, type, nar, args, o };
-  o[3].node.addEventListener(type, listener);
-  C(oc.start, listener);
-}
-function end(o) {
-  console.log("removeEventListener", o[3].node.nodeName);
-  o[3].node.removeEventListener(o.type, o);
-}
-function insertBefore(o) {
-  console.log("insertBefore", o);
-}
-function example(o) {
-  C(o.r, "hey");
-  const on = npith(document.body);
-  const oc = {
-    ds: new Set(),
-    start(o, d) {
-      o.ds.add(d);
-      console.log("D", d);
-    },
-    end(o) {
-      for (let d of o.ds) C(d.end);
-      o.ds.clear();
-    },
-  };
-  C(on.start, oc, "mousemove", (o, e) => {
-    console.log(e.x, e.y);
-  });
-  C(on.start, oc, "click", (o, e) => {
-    console.log("click");
-    C(oc.end);
-  });
-}
-//example(lr_pith(), [], 0, [], 0);
