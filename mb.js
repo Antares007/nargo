61bf2e804120f9265143e5eb76b277eb807da9bc
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-05-01 15:13:17 +0400

    mb

--- mb.js
+++ mb.js
@@ -3,12 +3,42 @@ const C3o1 = (o) => C(o[3].o[1]);
 const C3o2 = (o) => C(o[3].o[2]);
 const cb = (o) => {
   const b = o[3].b;
-  C(b, o[3].o);
+  C(b, o[3].o, ...o[3].ba);
 };
-const mb0 = (o, a, b) => C(a, [cb, C3o1, C3o2, { o, b }]);
-const mb1 = (o, a, b) => C(a, [C3o0, cb, C3o2, { o, b }]);
-const mb2 = (o, a, b) => C(a, [C3o0, C3o2, cb, { o, b }]);
-module.exports = { mb0, mb1, mb2, cb, C3o0, C3o1, C3o2 };
+const cc = (o) => {
+  const c = o[3].c;
+  C(c, o[3].o, ...o[3].ca);
+};
+const mb0 = (o, a, b) => C(a, [cb, C3o1, C3o2, { o, b, ba: [] }]);
+const mb1 = (o, a, b) => C(a, [C3o0, cb, C3o2, { o, b, ba: [] }]);
+
+const mb2 = (o, a, b1, b2, b) => C(a, [C3o0, C3o2, cb, { o, b, ba: [b1, b2] }]);
+const mb33 = (o, a, b1, b2, b3, b, c1, c2, c3, c) =>
+  C(a, [cb, cc, C3o2, { o, b, c, ba: [b1, b2, b3], ca: [c1, c2, c3] }]);
+const mb01 = (o, a, b, c1, c) =>
+  C(a, [cb, cc, C3o2, { o, b, c, ba: [], ca: [c1] }]);
+const mb11 = (o, a, b1, b, c1, c) =>
+  C(a, [cb, cc, C3o2, { o, b, c, ba: [b1], ca: [c1] }]);
+const mb23 = (o, a, b1, b2, b, c1, c2, c3, c) =>
+  C(a, [cb, cc, C3o2, { o, b, c, ba: [b1, b2], ca: [c1, c2, c3] }]);
+const mb32 = (o, a, b1, b2, b3, b, c1, c2, c) =>
+  C(a, [cb, cc, C3o2, { o, b, c, ba: [b1, b2, b3], ca: [c1, c2] }]);
+module.exports = {
+  0: mb0,
+  11: mb11,
+  mb0,
+  mb1,
+  mb2,
+  mb01,
+  mb11,
+  mb33,
+  mb23,
+  mb32,
+  cb,
+  C3o0,
+  C3o1,
+  C3o2,
+};
 const example = (o) => {
   const one = (o) => C(o[0], 1);
   const exp = (o) =>
--- npith.js
+++ npith.js
@@ -1,4 +1,5 @@
-const { mb0, mb1, cb, C3o0, C3o1, C3o2 } = require("./mb");
+const { mb0, mb2, mb11, mb23, cb, C3o0, C3o1, C3o2 } = require("./mb");
+const mb = require("./mb");
 const example0 = (o) => {
   C(o[0], 1, 2, 3);
   const pre = document.createElement("pre");
@@ -6,76 +7,110 @@ const example0 = (o) => {
   pre.insertBefore(textnode, null);
   document.body.insertBefore(pre, document.body.childNodes[0]);
   C(mb0, o, textnode, createTextNode, (o, ot) => {
-    C(ot[0], "a", 1, 2);
-    C(ot[0], "b");
-    C(ot[0], "c");
+    C(ot[0], "a", 1, 2, 3);
+    C(ot[1], "b", 1, 2, 3);
+    C(ot[1], "a", 1, 2, 3);
+    C(ot[0], "a", 1, 2, 3);
+    C(ot[1], "a", 1, 2, 3);
+    C(ot[1], "b", 1, 2, 3);
     C(ot[2]);
-    setTimeout(() => {
-      C(ot[0], "b", 1);
-      C(ot[0], "a", 1, 2);
-    }, 1000);
+    //C(ot[2]);
+    C(ot[1], "b", 1, 2, 3);
+    C(ot[0], "a", 1, 2, 3);
   });
 };
 setTimeout(() => {
   example0([(o, ...args) => console.log(args), , {}], [], 0);
 }, 0);
-function createTextNode_Ray0(o, ...args) {
-  const { piths, pc } = o[3];
-  let index = null;
-  for (let i = pc, l = piths.length; index == null && i < l; i++) {
-    const [sargs] = piths[i];
-    let equal = sargs.length === args.length;
-    for (let i = 0, l = sargs.length; equal && i < l; i++)
-      equal = sargs[i] === args[i];
-    if (equal) index = i;
-  }
-  for (let i = pc - 1; index == null && -1 < i; i--) {
-    const [sargs] = piths[i];
-    let equal = sargs.length === args.length;
-    for (let i = 0, l = sargs.length; equal && i < l; i++)
-      equal = sargs[i] === args[i];
-    if (equal) index = i;
-  }
-  if (index == null) piths.splice(pc, 0, [args, 0]), o[3].pc++;
-  else {
-    piths[index][1]++;
-    if (index < pc)
-      index < pc - 1 && piths.splice(pc - 1, 0, piths.splice(index, 1)[0]);
-    else if (pc < index)
-      piths.splice(pc, 0, piths.splice(index, 1)[0]), o[3].pc++;
-    else o[3].pc++;
-  }
-  C(createTextNode_draw, o);
+
+function findIndex(o, p, o3, eq) {
+  const { piths, pc } = o3;
+  for (let i = pc, l = piths.length; i < l; i++)
+    if (eq(piths[i], p)) {
+      C(o[0], i, o3);
+      return;
+    }
+  for (let i = pc - 1; -1 < i; i--)
+    if (eq(piths[i], p)) {
+      C(o[0], i, o3);
+      return;
+    }
+  C(o[1], p, o3);
 }
-function createTextNode_draw(o) {
-  o[3].node.textContent = o[3].piths
-    .slice(0, o[3].pc)
-    .map(
-      (args) =>
-        (args[1] ? `(${args[1] + 1})` : "") +
-        args[0].map((a) => a + "").join(", ") +
-        ";"
-    )
+function insert(o, p, o3, cr) {
+  o3.piths.splice(o3.pc, 0, cr(p));
+  o3.pc++;
+  C(o[0], o3);
+}
+function reinsert(o, index, o3, up) {
+  const { piths, pc } = o3;
+  up(piths[index]);
+  if (index < pc) {
+    index < pc - 1 && piths.splice(pc - 1, 0, piths.splice(index, 1)[0]);
+  } else if (pc < index) {
+    piths.splice(pc, 0, piths.splice(index, 1)[0]);
+    o3.pc++;
+  } else {
+    o3.pc++;
+  }
+  C(o[0], o3);
+}
+function draw(o, o3) {
+  o3.node.textContent = o3.piths
+    .slice(0, o3.pc)
+    .map((a) => (a[0] === 1 ? a[1] : `(${a[0]})` + a[1]))
     .join("\n");
 }
-function createTextNode_Ray2(o) {
-  const { piths, pc } = o[3];
-  for (let l = piths.length; l > pc; l--) piths.splice(pc, 1)[0];
-  o[3].pc = 0;
-  C(createTextNode_draw, o);
+
+function create(a) {
+  return [1, a];
+}
+function update(a) {
+  return a[0]++, a;
+}
+function equal(a, b) {
+  return a[1] === b;
+}
+function run(o, n) {
+  C(n, o);
+}
+// prettier-ignore
+function ray(o, p, o3) {
+  C(run, o, p, o3, equal, findIndex, update, reinsert, create, insert, mb[11], draw, mb[0]);
+}
+function ray0(o, ...p) {
+  C(ray, o, p.join(", "), o[3]);
+}
+function ray1(o, ...p) {
+  C(ray, o, "Error:" + p.join(", "), o[3]);
 }
 function createTextNode(o, node) {
-  C(o[0], [
-    createTextNode_Ray0,
-    ,
-    createTextNode_Ray2,
-    {
-      node,
-      piths: [],
-      pc: 0,
-    },
-  ]);
+  C(o[0], [ray0, ray1, ray2, { node, piths: [], pc: 0 }]);
 }
+function ray2(o) {
+  const { piths, pc } = o[3];
+  o[3].pc = 0;
+  piths.splice(pc, piths.length - pc);
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 function npith(o, node) {
   if (node.nodeType === 1) {
     const piths = [];
