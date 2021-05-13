202cd0b588f0f94a163a3e90b2990f36d4030827
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-05-13 10:47:10 +0400

    mbo

--- g.js
+++ g.js
@@ -1,3 +1,4 @@
+const mbo = require("./mbo");
 function C(...args) {}
 /*
           E → E + T               E
@@ -12,7 +13,6 @@ function C(...args) {}
   E → T * (plus * E * _add + ε)
   T → F * (star * T * _mul + ε)
   F → ( * E * ) * _31 + ds * _ds
-  
 */
 
 function E() {
@@ -32,21 +32,41 @@ function E() {
   const star = (o) => C(cp, o, "*".codePointAt(0));
   const plus = (o) => C(cp, o, "+".codePointAt(0));
 
+  const _if = (o) => C(str, o, "if");
+  const _then = (o) => C(str, o, "if");
+  const _else = (o) => C(str, o, "if");
+  var S = ws * _if * E;
   return E;
-}
-function ws(o, p, i) {
-  while (i[p] === " " || i[p] === "\t" || i[p] === "\n" || i[p] === "\r") p++;
-  C(o[0], p, i);
 }
 function example(o) {
   const ε = (o) => C(o[0]);
-  C(E(), o, 0, ` ( 3 + 6 ) * 9 `);
+  C(E(), o, 0, `(3+6)*9`);
   var AB = a + b;
   var ABS = AB * ABS + ε;
   C(ABS, o, 0, "baababb");
   var as = a * as;
   var S = b * as + ε;
   C(S, o, 0, "baaaaaa");
+}
+function range(o, p, i, f, t) {
+  const cp = i.codePointAt(p) | 0;
+  if (cp < f || t < cp) C(o[1], p, i);
+  else C(o[0], cp, p + 1, i);
+}
+function cp(o, p, i, cp) {
+  if (cp === (i.codePointAt(p) | 0)) C(o[0], cp, p + 1, i);
+  else C(o[1], p, i);
+}
+function str(o, pos, inp, match) {
+  const npos = pos + match.length;
+  if (inp.length < npos) return C(o[1], pos, inp);
+  for (let i = 0, l = match.length; i < l; i++)
+    if (inp[pos + i] !== match[i]) return C(o[1], pos, inp);
+  C(o[0], npos, inp);
+}
+function ws(o, p, i) {
+  while (i[p] === " " || i[p] === "\t" || i[p] === "\n" || i[p] === "\r") p++;
+  C(o[0], p, i);
 }
 function b(o) {
   C(cp, o, 98);
@@ -64,63 +84,3 @@ function a(o) {
   [],
   0
 );
-function range(o, p, i, f, t) {
-  const cp = i.codePointAt(p);
-  if (cp < f || t < cp) C(o[1], p, i);
-  else C(o[0], cp, p + 1, i);
-}
-function cp(o, p, i, cp) {
-  if (cp === i.codePointAt(p)) C(o[0], cp, p + 1, i);
-  else C(o[1], p, i);
-}
-function nar(o, nar) {
-  C(nar, o);
-}
-function cao(o) {
-  const a = o[3].a;
-  C(a, o[3].o);
-}
-function cbo(o) {
-  const b = o[3].b;
-  C(b, o[3].o);
-}
-function cco(o) {
-  const c = o[3].c;
-  C(c, o[3].o);
-}
-function co3o0(o) {
-  C(o[3].o[0]);
-}
-function co3o1(o) {
-  C(o[3].o[1]);
-}
-function co3o2(o) {
-  C(o[3].o[2]);
-}
-function ma(o, m, a) {
-  C(m, [cao, co3o1, co3o2, { o, a }]);
-}
-function mb(o, m, b) {
-  C(m, [co3o0, cbo, co3o2, { o, b }]);
-}
-function mab(o, m, a, b) {
-  C(m, [cao, cbo, co3o2, { o, b, a }]);
-}
-///////////////////////////////////////////
-function c20(o) {
-  C(o[2][0]);
-}
-function c21(o) {
-  C(o[2][1]);
-}
-function cbo2(o) {
-  const b = o[3];
-  C(b, o[2], ...o[4]);
-}
-function mbo(o, b, r, n, ...args) {
-  const nargs = args.splice(args.length - n, n);
-  const nar = args.pop();
-  const pith = [c20, c21, o, b, nargs];
-  pith[r] = cbo2;
-  C(nar, pith, ...args);
-}
--- g2.js
+++ g2.js
@@ -1,13 +1,4 @@
-const rays = [
-  (o) => C(o[o.length - 1][0][0]),
-  (o) => C(o[o.length - 1][0][1]),
-  (o) => C(o[o.length - 1][0][2]),
-  (o) => C(o[o.length - 1][0][3]),
-  (o) => C(o[o.length - 1][0][4]),
-  (o) => C(o[o.length - 1][0][5]),
-  (o) => C(o[o.length - 1][0][6]),
-  (o) => C(o[o.length - 1][0][7]),
-];
+const mbo = require("./mbo");
 function example(o) {
   function one(o) {
     C(o[0], 1);
@@ -58,20 +49,3 @@ const o = [
   {},
 ];
 example(o, [], 0);
-
-function cbo(o) {
-  const s = o[o.length - 1];
-  const b = s[1];
-  C(b, s[0], ...s[2]);
-}
-function mbo(o, b, r, n, ...args) {
-  const nargs = args.splice(args.length - n, n);
-  const nar = args.pop();
-  const length = o.length;
-  const pith = Array(length);
-  const spos = length - 1;
-  for (let i = 0; i < spos; i++) pith[i] = rays[i];
-  pith[spos] = [o, b, nargs];
-  pith[r] = cbo;
-  C(nar, pith, ...args);
-}
