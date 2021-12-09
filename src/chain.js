692da3fcd3fd53c617e4f1b91d79e7c0f509ced3
Archil Bolkvadze <a.bolkvadze@gmail.com>
2021-12-09 23:13:01 +0400

    fix chain

--- src/chain.js
+++ src/chain.js
@@ -1,18 +1,18 @@
 const { mb, o, rg } = require("./mbo");
 
-const n1 = (o) => Go(o.v, 3);
+const n1 = (o) => C(o.v, 3);
 const n2 = (o, ...nargs) => (
-  Go(o.v, ...nargs, "α"),
-  Go(o.v, ...nargs, "α", "β"),
-  Go(o.v, ...nargs, "α", "β", "ο")
+  C(o.v, ...nargs, "α"),
+  C(o.v, ...nargs, "α", "β"),
+  C(o.v, ...nargs, "α", "β", "ο")
 );
-const n3 = (o, ...h) => Go(o.v, ...h, 6);
-const n4 = (o, a) => Go(o.v, a, a + 3);
-const ne = (o) => Go(o.e, "e");
+const n3 = (o, ...h) => C(o.v, ...h, 6);
+const n4 = (o, a) => C(o.v, a, a + 3);
+const ne = (o) => C(o.e, "e");
 const lo = (o) => (
-  console.log("b", o.b.name), Go(o.v), console.log("e", o.b.name)
+  console.log(o.b.name + ">"), C(o.v), console.log("<" + o.b.name)
 );
 
-Go(mb, o, n1, n2, mb, n3, mb, n4);
+C(mb, o, n1, n2, mb, n3, mb, n4);
 console.log("***");
-Go(rg, o, n1, n2, mb, n3, mb, n4, mb, lo);
+C(rg, o, n1, n2, mb, n3, mb, n4, mb, lo);
